5d48bdda3e06d68791c21b9886ed144ca0283d7b
Annamaria Mazzia <annamaria.mazzia@unipd.it>
2026-01-04 19:28:47 +0100

    Update script_trefoilmotif.m

--- script_trefoilmotif.m
+++ script_trefoilmotif.m
@@ -19,24 +19,20 @@ plot(xv,yv,'bo')
 % preallocation of variables xu, yu and t
 xu=zeros(size(xv)); yu=xu; t=xu;
 for i=1:3
+    indm=mod(i-2,3)+1;
     % indm - previous index along the circle
-     if i==1 
-         indm=3;  
-     else
-         indm=i-1;
-     end   
     % centroids of the three circles
     xu(i)=(xv(i))/2;
     yu(i)=(yv(i))/2;
-    t(i)=angle(complex(xv(indm),yv(indm)))+pi/3;  % select the angle 
-    % for drawing the circles
+    t(i)=atan2(yv(indm),xv(indm)); 
+   % select the angle  for drawing the circles
     plot(xu(i),yu(i),'ro')
 end
  r1=norm([xu(1),yu(1)]-[xv(1) yv(1)]); % radius of the three circles
 A=[]; % matrix to store the coordinates of the trefoil curve
 % plot of the three circles
  for i=1:3
-     theta2=linspace(t(i)-pi/3, t(i)+pi);
+     theta2=linspace(t(i), t(i)+4*pi/3);
      xi=xu(i)+r1*cos(theta2);
      yi=yu(i)+r1*sin(theta2);
      plot(xi,yi,'b','LineWidth',2,'LineJoin', 'chamfer')
