cd368b6218b85ae045bd64e7d1a280ea53d75f92
Annamaria Mazzia <annamaria.mazzia@unipd.it>
2026-01-04 17:37:36 +0100

    Update trileaves.m

--- trileaves.m
+++ trileaves.m
@@ -1,14 +1,35 @@
-function  [sc,tc]=trileaves(n,rold,stringa)
-theta1=linspace(pi/2,pi/2+2*pi,2*n+1);
-m=2*n;
-xx=rold*cos(theta1(1:2:m)); yy=rold*sin(theta1(1:2:m));
-rc1=norm([xx(1) yy(1)]-[xx(2) yy(2)]);
-j=1;
-for i=1:n
-    thetac=linspace(theta1(j)-3*pi/4+0.2, theta1(j)+3*pi/4-0.1);
-    sc=xx(i)+rc1*cos(thetac);
-    tc=yy(i)+rc1*sin(thetac);
-    plot(sc,tc,'Color',stringa,'LineWidth',2,'LineJoin', 'chamfer')
-    j=j+2;
-end
+function [sc, tc] = trileaves(n, rold, stringa)
+theta1 = linspace(pi/2, pi/2 + 2*pi, n + 1);
+% coordinates of n equidistant points along the circle
+% of radius rold
+xx = rold*cos(theta1(1:n)); 
+yy = rold*sin(theta1(1:n));
+% rc1 is the radius of the arc of circle that will
+% be constructed on each point (xx(i),yy(i)) 
+% starting from the previous point (xx(prev),yy(prev)) 
+% and going to the next point (xx(next), yy(next))
+rc1 = norm([xx(1) yy(1)] - [xx(2) yy(2)]);
+hold on
+for i = 1:n
+    % next and prev are the next and previous indices to respect
+    % to index i
+    next = mod(i,n) + 1;
+    prev=mod(i-2,n)+1;
+    %t0 and t1 are the first and last angle to be used to
+    % generate the arc of circle. 
+    % t0 is the angle between points i and prev
+    % t1 is the angle between points i and next
+    t0=  atan2(yy(prev) - yy(i), xx(prev) - xx(i));
+    t1 = atan2(yy(next) - yy(i), xx(next) - xx(i));
+    % we make this check to be sure that the rotation is
+    % counterclockwise
+    if t1< t0
+        t1 = t1 + 2*pi;
+    end
+    theta2 = linspace(t0, t1);
+    sc = xx(i) + rc1 * cos(theta2);
+    tc = yy(i) + rc1 * sin(theta2);
+    plot(sc, tc, 'Color', stringa, 'LineWidth', 2,'LineJoin','chamfer')
+    hold on
+ end
 end
