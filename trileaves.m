61cfde1a106f4fda9355c63a910415698c6ab90f
Annamaria Mazzia <annamaria.mazzia@unipd.it>
2026-01-04 17:40:42 +0100

    Update trileaves.m

--- trileaves.m
+++ trileaves.m
@@ -4,15 +4,13 @@ theta1 = linspace(pi/2, pi/2 + 2*pi, n + 1);
 % of radius rold
 xx = rold*cos(theta1(1:n)); 
 yy = rold*sin(theta1(1:n));
-% rc1 is the radius of the arc of circle that will
-% be constructed on each point (xx(i),yy(i)) 
-% starting from the previous point (xx(prev),yy(prev)) 
-% and going to the next point (xx(next), yy(next))
+% rc1 is the radius of the circular arc constructed at each point 
+% (xx(i), yy(i)).
 rc1 = norm([xx(1) yy(1)] - [xx(2) yy(2)]);
 hold on
 for i = 1:n
-    % next and prev are the next and previous indices to respect
-    % to index i
+    % next and prev are the indices 
+    % of the neighboring points relative to index i
     next = mod(i,n) + 1;
     prev=mod(i-2,n)+1;
     %t0 and t1 are the first and last angle to be used to
@@ -21,8 +19,7 @@ for i = 1:n
     % t1 is the angle between points i and next
     t0=  atan2(yy(prev) - yy(i), xx(prev) - xx(i));
     t1 = atan2(yy(next) - yy(i), xx(next) - xx(i));
-    % we make this check to be sure that the rotation is
-    % counterclockwise
+    % Ensure the arc is drawn in a counter-clockwise direction
     if t1< t0
         t1 = t1 + 2*pi;
     end
