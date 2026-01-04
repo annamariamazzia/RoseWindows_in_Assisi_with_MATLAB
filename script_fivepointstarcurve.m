96640db925fd3169db3c62505dc74b59e88ddbe0
Annamaria Mazzia <annamaria.mazzia@unipd.it>
2026-01-04 18:20:21 +0100

    Update script_fivepointstarcurve.m

--- script_fivepointstarcurve.m
+++ script_fivepointstarcurve.m
@@ -17,30 +17,22 @@ hold on
 % selection of five points along the circle
 n=5;
 theta1=linspace(pi/2,pi/2+2*pi,n+1);
-xx=cos(theta1(1:n)); yy=sin(theta1(1:n));
-plot(xx,yy,'o')
+xc=cos(theta1(1:n)); yc=sin(theta1(1:n));
+plot(xc,yc,'k^')
 % The vertices of a pentagon, which will be the centers of the five 
 % circles used to construct the overall curve.
-xu=xx/2; 
-yu=yy/2;
-plot(xu,yu,'m*')
-r1=norm([xu(1),yu(1)]-[xx(1) yy(1)]); 
+xu=xc/2; 
+yu=yc/2;
+plot(xu,yu,'k*')
+r1=norm([xu(1),yu(1)]-[xc(1) yc(1)]); 
 r1=r1/2;  % radius of the circles
 A=[]; % Matrix to store the curve points
 for i=1:n
-     % Assuming the centers are ordered anti-clockwise, indm denotes 
+     % Assuming the centers are ordered counter-clockwise, indm denotes 
      % the index of the preceding center and indp the index of 
-     % the succeeding center
-     if i==1 
-         indm=n;
-     else
-         indm=i-1;
-     end  
-     if i==n
-         indp=1;
-     else
-         indp=i+1;
-     end
+     % the next center
+     indm=mod(i-2,n)+1;
+     indp=mod(i,n)+1;
      % We draw the circles external to the pentagon so that they are 
      % connected by the segments  of the pentagon that remain  unused. 
      % We must, therefore, determine the intersection points between 
@@ -58,8 +50,8 @@ for i=1:n
      else
          xx=x2; yy= m*(x2-xu(i))+yu(i);
      end
-     t2=angle(complex(xx,yy));  % The angle of the vector from the origin 
-     % (0,0) to the intersection point.
+     t2=  atan2(yu(indp) - yy,xu(indp) - xx);
+     % t2  The angle of the vector from the center to the intersection point.
      m=(yu(indm)-yu(i))/(xu(indm)-xu(i));  %The slope of the line connecting 
      % the i-th center with the previous one
      % x1 and x2 are the  x-coordinates of the intersection  points 
@@ -72,23 +64,25 @@ for i=1:n
      else
          xxx=x2; yyy= m*(x2-xu(i))+yu(i);
      end
-     t1=angle(complex(xxx,yyy)); % The angle of the vector from the origin 
-     % (0,0) to the intersection point.
+     t1=  atan2(yu(indm) - yyy, xu(indm) - xxx);
+     % t1 The angle of the vector from the center
+     %  to the intersection point.
      %
-     % theta2=  the range in which the angle t1 must 
+     % theta2 is the range in which the angle t1 must 
      % vary to obtain the correct portion of the circle.
-      if t1<t2
-       theta2=linspace(t1-pi/2,t2+pi/2);
-      else
-        theta2=linspace(t1-pi/2,2*pi+t2+pi/2);
+     % 
+     % Ensure the arc is drawn in a counter-clockwise direction
+      if t2<t1
+          t2=t2+2*pi;
       end
-      % (xi,yi): points along the arc to be drawn
+      theta2=linspace(t1,t2);
+     % (xi,yi): points along the arc to be drawn
      xi=xu(i)+r1*cos(theta2);
      yi=yu(i)+r1*sin(theta2);
      plot(xi,yi,'color',stringa,'LineWidth',2,'LineJoin', 'chamfer')
-     plot(xi(1),yi(1),'ko')
-     plot(xx,yy,'bo')
-     plot(xxx,yyy,'g*')
+     plot(xi(1),yi(1),'go')
+     plot(xx,yy,'bd')
+     plot(xxx,yyy,'gd')
      plot([xu(i) xx xu(indp)], [yu(i) yy yu(indp)],'b')
      plot([xu(i) xxx xu(indm)], [yu(i) yyy yu(indm)],'g')
      if isempty(A)
@@ -99,5 +93,6 @@ for i=1:n
 end
  A=[A(1,:) A(1,1); A(2,:) A(2,2)]; % Append the first point 
  % to the end of the matrix to close the shape.
- axis square
- save('pentastella.dat', 'A', '-ascii');
+ plot(A(1,:),A(2,:),'k-.','LineWidth',2,'LineJoin', 'chamfer')
+axis square
+save('pentastella.dat', 'A', '-ascii');
